f8956a7394624ec9b49cc12a7c5d9c2b1c7ce9fb
D Cameron Mauch <cmauch@enbala.com>
2016-10-03 14:36:56 -0600

    using double int to get around javascript integer size limitation

--- 1_algorithmic_toolbox/assignment_2_4.js
+++ 1_algorithmic_toolbox/assignment_2_4.js
@@ -2,6 +2,46 @@ const readline = require('readline');
 
 process.stdin.setEncoding('utf8');
 
+class DoubleInt {
+  constructor(low, high = 0) {
+    this.places = 15;
+    this.split = Math.pow(10, this.places);
+    this.high = high + Math.floor(low / this.split);
+    this.low = low % this.split;
+  }
+
+  static add(a, b) {
+    const low = (a.low + b.low) % a.split;
+    const high = a.high + b.high + Math.floor((a.low + b.low) / a.split);
+    return new DoubleInt(low, high);
+  }
+
+  static subtract(a, b) {
+    const low = (a.split + a.low - b.low) % a.split;
+    const high = a.high - b.high - 1 + Math.floor((a.split + a.low - b.low) / a.split);
+    return new DoubleInt(low, high);
+  }
+
+  static multiply(a, b) {
+    const low = (a.low * b.low) % a.split;
+    const high = (a.high * b.high * a.split) +
+                 (a.high * b.low) +
+                 (a.low * b.high) +
+                 Math.floor((a.low * b.low) / a.split);
+    return new DoubleInt(low, high);
+  }
+
+  get toString() {
+    if (this.high > 0) {
+      const lowStr = this.low.toString();
+      return this.high.toString() + "0".repeat(this.places - lowStr.length) + lowStr;
+    } else {
+      return this.low.toString();
+    }
+    return `foo`
+  }
+}
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -10,7 +50,7 @@ const rl = readline.createInterface({
 
 const handler = (line) => {
   const [x, y] = line.split(" ").map((v) => parseInt(v, 10));
-  console.log(lcm(x, y));
+  console.log(lcm(x, y).toString);
   rl.close();
 };
 
@@ -23,7 +63,7 @@ const gcd = (x, y) => {
 
 const lcm = (x, y) => {
   const z = gcd(x, y);
-  return (x / z) * (y / z) * z;
+  return DoubleInt.multiply(new DoubleInt(x / z), new DoubleInt(y));
 }
 
 rl.on('line', handler);
